73a58199f6e5faac6a8e7266b1b02d0cd6466b12
piggsoft@163.com <piggsoft@163.com@fad789e4-54dc-ac77-6dee-d2d68ab71792>
2013-05-22 14:31:54 +0000

    [Modify] 1.去除DialectHelper 2.新增DialectFactory 3.使用简单工厂模式使以后加入dialect只需更改dialect.properties

---
+++ trunk/src/main/java/com/piggsoft/comms/mybatis/dialect/DialectFactory.java
@@ -0,0 +1,41 @@
+package com.piggsoft.comms.mybatis.dialect;
+
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.piggsoft.comms.utils.StringUtils;
+
+/**
+ * @author CharChen
+ * @e-mail piggsoft@163.com
+ * @since 2013-5-22
+ * @version 1.0
+ */
+public class DialectFactory {
+	
+	private final static Logger logger = Logger.getLogger(DialectFactory.class.getName());
+	
+	private static final Properties props = new Properties();
+	
+	static {
+		try {
+			props.load(DialectFactory.class.getResourceAsStream("dialect.properties"));
+		} catch (IOException e) {
+			logger.log(Level.SEVERE, e.getMessage(), e);
+		}
+	}
+	
+	public static Dialect createDialect(String dialectString) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+		if(StringUtils.isBlank(dialectString)) {
+			throw new RuntimeException("please designation the dialect");
+		}
+		String className = props.getProperty(dialectString.toUpperCase());
+		if (StringUtils.isBlank(className)) {
+			throw new RuntimeException("do not support this dialect " + dialectString);
+		}
+		Dialect dialect = (Dialect)Class.forName(className).newInstance();
+		return dialect;
+	}
+}
